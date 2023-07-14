e664e3c1216e3f66f17678ecb646643cc06035aa
Tuanemendes <tuane.belmiro@gmail.com>
2023-07-14 18:33:47 -0300

    ajuste na tela de descrição do produto

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -21,42 +21,53 @@ export default function ProductScreen() {
         <div className='py-2 text-brow-coffee'>
             <Link href='/'>Voltar para produtos</Link>
         </div>
-        <div className='grid md:grid-cols-4 md:gap-3'>
-            <div className='md:col-span-2'>
+        <div className='card p-5 shadow-md '>
+        <div className='grid md:grid-cols-2 md:gap-10'>
+            <div className='md:col-span-1'>
                 <Image
                 src = {product.image}
                 alt = {product.name}
                 width = {640}
                 height = {640}
                 layout = 'responsive'
+                className='rounded-xl shadow-md'
                 ></Image>         
             </div>
-            <div className=' text-brow-coffee '>
-               <ul>
-                <li>
-                <h1 className='text-lg'>{product.name}</h1>
-                </li>
+            <div>
+            <div className='p-5 text-brow-coffee '>
+                <ul>
+                <li><h1 className='text-xl'>{product.name}</h1></li>
+                <li>{product.rating} Estrelas ({product.numReviews} Avaliações)</li>
                 <li>Categoria: {product.category}</li>
                 <li>Tipo: {product.type}</li>
-                <li>{product.rating} Estrelas ({product.numReviews} Avaliações)</li>
-                <li>Descrição: {product.description}</li>
+                </ul>
+            </div>
+            <div className='card p-5   text-brow-coffee'>
+                <div className='mb-2 flex justify-between'>
+                    <div>Preço</div>
+                    <div>R${product.price}</div>
+                </div>
+                <div className='mb-2 flex justify-between'>
+                    <div>Status</div>
+                    <div>{product.countInStock > 0 ? 'Produto Disponível' : 'Produto Indisponível'}</div>
+                </div>
+                <div className=' flex justify-between'>
+                <button className='primary-button m-1'>Adicionar ao carrinho</button>
+                <button className='primary-button m-1'>
+                <Link href='/'>Comprar</Link>
+                </button>
+                </div>
+            </div>    
+            <div className='p-5 text-brow-coffee '>
+               <ul>   
+           
+                <li><h1 className='text-lg'>Descrição</h1> {product.description}</li>
                </ul> 
             </div>
-            <div>
-                <div className='card p-5  text-brow-coffee'>
-                    <div className='mb-2 flex justify-between'>
-                        <div>Preço</div>
-                        <div>R${product.price}</div>
-                    </div>
-                    <div className='mb-2 flex justify-between'>
-                        <div>Status</div>
-                        <div>{product.countInStock > 0 ? 'Produto Disponível' : 'Produto Indisponível'}</div>
-                    </div>
-                    <button className='primary-button w-full'>Adicionar ao carrinho</button>
-                </div>
             </div>
-            
         </div>
+        </div>
+     
     </Layout>
 
 
